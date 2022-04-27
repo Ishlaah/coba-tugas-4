2e3009f428fff6d81b0b7b6f9227ee0e8b46beea
Ishlaah <muhammadsyabani5@gmail.com>
2022-04-28 00:27:59 +0700

    hehe

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import GUI.GUI;
+
+public class Main {
+    public static void main(String[] args) {
+        // Connector connection = new Connector();
+        GUI gui = new GUI();
+        gui.initialize();
+    }
+}
